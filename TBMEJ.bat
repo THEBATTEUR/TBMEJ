f5529c2b9c0cfe2da91f6aa30c0e7da908603a88
THE_BATTEUR <nicomaill74@gmail.com>
2022-04-19 23:48:36 +0200

    Ajout fichiers de lancement pour Windows & Linux

---
+++ TBMEJ.bat
@@ -0,0 +1,6 @@
+@echo off
+title MATh en JEANS : Traceur-Briseur
+TBMEJ.exe
+echo.
+echo Appuyez sur une touche pour fermer...
+pause>nul
---
+++ TBMEJ.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# TBMEJ.sh
+echo MATh en JEANS : Traceur-Briseur
+echo ""
+read -s -n 1 -p "Appuyez sur une touche pour fermer..."
+echo ""
+exit
